4e1d6184a2131af86df5e8ece84604d975096396
3Mustard <m23.cullen@gmail.com>
2020-07-17 15:57:16 -0400

    corrected capitalization mistake

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -17,14 +17,14 @@ class Register extends React.Component {
             Register for app
           </Header>
           <Form size='large'>
-            <segment stacked>
+            <Segment stacked>
               <Form.Input fluid name='username' icon='user' iconPosition='left' placeholder='Username' onChange={this.handleChange} type='text'/>
               <Form.Input fluid name='email' icon='mail' iconPosition='left' placeholder='Email Address' onChange={this.handleChange} type='email'/>
               <Form.Input fluid name='password' icon='lock' iconPosition='left' placeholder='Password' onChange={this.handleChange} type='password'/>
               <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left' placeholder='Password Confirmation' onChange={this.handleChange} type='password'/>
 
               <Button color='orange' fluid size='large'>Submit</Button>
-            </segment>
+            </Segment>
           </Form>
           <Message>
             Already a user? <Link to='/login'>Login</Link>
