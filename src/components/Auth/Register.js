2f17c6f149e4685c86eedd74bda648c28090032b
3Mustard <m23.cullen@gmail.com>
2020-07-17 15:55:39 -0400

    built register form. Added custom css for register form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,8 +1,37 @@
 import React from "react";
+import { Grid, Form, Segment, Button, Header, Message, Icon } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 class Register extends React.Component {
+
+  handleChange = () => {
+
+  }
+
   render() {
-    return <div>Register</div>;
+    return (
+      <Grid className='app' textAlign='center' verticalAlign='middle'>
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as='h2' icon color='orange' textAlign='center'>
+            <Icon name='puzzle piece' color='orange'/>
+            Register for app
+          </Header>
+          <Form size='large'>
+            <segment stacked>
+              <Form.Input fluid name='username' icon='user' iconPosition='left' placeholder='Username' onChange={this.handleChange} type='text'/>
+              <Form.Input fluid name='email' icon='mail' iconPosition='left' placeholder='Email Address' onChange={this.handleChange} type='email'/>
+              <Form.Input fluid name='password' icon='lock' iconPosition='left' placeholder='Password' onChange={this.handleChange} type='password'/>
+              <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left' placeholder='Password Confirmation' onChange={this.handleChange} type='password'/>
+
+              <Button color='orange' fluid size='large'>Submit</Button>
+            </segment>
+          </Form>
+          <Message>
+            Already a user? <Link to='/login'>Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    )
   }
 }
 
