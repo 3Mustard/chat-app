9b150214b028851c2e0955c07b3e0b5283209cae
3Mustard <m23.cullen@gmail.com>
2020-07-28 13:56:02 -0400

    added route with auth

--- src/index.js
+++ src/index.js
@@ -7,21 +7,35 @@ import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import registerServiceWorker from "./registerServiceWorker";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, withRouter } from "react-router-dom";
 
 class Root extends React.Component {
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
   render() {
     return (
-      <Router>
         <Switch>
           <Route exact path="/" component={App} />
           <Route path="/login" component={Login} />
           <Route path="/register" component={Register} />
         </Switch>
-      </Router>
     );
   }
 };
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>, 
+  document.getElementById("root")
+);
 registerServiceWorker();
