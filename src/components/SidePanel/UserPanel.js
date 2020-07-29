6684bdab3b14eb601a9d00e8ae1fabd42c7f4e8e
3Mustard <m23.cullen@gmail.com>
2020-07-29 15:23:06 -0400

    added avatar image to dropdown

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import firebase from '../../firebase';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
 
@@ -32,7 +32,7 @@ class UserPanel extends React.Component {
     }
 
     render() {
-        console.log(this.props.currentUser)
+        const { user } = this.state;
 
         return (
             <Grid style={{ background: '#4c3c4c' }}>
@@ -43,14 +43,17 @@ class UserPanel extends React.Component {
                             <Icon name="code" />
                             <Header.Content>Let's Trade</Header.Content>
                         </Header>
-                    </Grid.Row>
-
-                    {/* Dropdown */}
-                    <Header style={{ padding: '0.25em' }} as="h4" inverted>
-                        <Dropdown trigger={
-                            <span>{this.state.user.displayName}</span>
-                        } options={this.dropDownOptions()} />
+                        {/* Dropdown */}
+                        <Header style={{ padding: '0.25em' }} as="h4" inverted>
+                            <Dropdown trigger={
+                                <span>
+                                    <Image src={user.photoURL} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>} 
+                                options={this.dropDownOptions()} 
+                            />
                     </Header>
+                    </Grid.Row>
                 </Grid.Column>
             </Grid>
         )
