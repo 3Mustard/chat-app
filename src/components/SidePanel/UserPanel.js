87f77a0e421d43c7c51c9d36cb8fd8217522e1b4
3Mustard <m23.cullen@gmail.com>
2020-07-29 14:18:57 -0400

    added dropdown to html in userpanel

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Header, Icon } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
     render() {
@@ -7,11 +7,19 @@ class UserPanel extends React.Component {
             <Grid style={{ background: '#4c3c4c' }}>
                 <Grid.Column>
                     <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
+                        {/* APP header */}
                         <Header inverted floated="left" as="h2">
                             <Icon name="code" />
                             <Header.Content>Let's Trade</Header.Content>
                         </Header>
                     </Grid.Row>
+
+                    {/* Dropdown */}
+                    <Header>
+                        <Dropdown trigger={
+                            <span>User</span>
+                        } options={this.dropDownOptions()} />
+                    </Header>
                 </Grid.Column>
             </Grid>
         )
