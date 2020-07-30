0a6f597fb9c1a745f07fad27b380832058d13321
3Mustard <m23.cullen@gmail.com>
2020-07-30 17:03:46 -0400

    made function addChannel to add channel from form to firebase

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -1,19 +1,52 @@
 import React from 'react';
+import firebase from '../../firebase';
 import { Menu, Icon, Modal, Form, Input, Button } from 'semantic-ui-react';
 
 class Channels extends React.Component {
 
   state = {
+    user: this.props.currentUser,
     channels: [],
     channelName: '',
     channelDetails: '',
+    channelsRef: firebase.database().ref('channels'),
     modal: false
+  };
+
+  addChannel = () => {
+    const { channelsRef, channelName, channelDetails, user } = this.state;
+    const key = channelsRef.push().key;
+
+    const newChannel = {
+      id: key,
+      name: channelName,
+      details: channelDetails,
+      createdBy: {
+        name: user.displayName,
+        avatar: user.photoURL
+      }
+    };
+
+    channelsRef
+      .child(key)
+      .update(newChannel)
+      .then(() => {
+        this.setState({
+          channelName: '',
+          channelDetails: ''
+        });
+        this.closeModal();
+        console.log('channel added');
+      })
+      .catch(err => {
+        console.error(err);
+      })
   }
 
   handleSubmit = event => {
     event.preventDefault();
     if (this.isFormValid(this.state)) {
-      console.log('channel added');
+      this.addChannel();
     }
   }
 
