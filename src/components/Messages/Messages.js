b5b70e3df1b12ff05293d0166a91636790618821
3Mustard <m23.cullen@gmail.com>
2020-07-31 14:41:48 -0400

    add message form to messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MessagesHeader from './MessagesHeader';
+import MessageForm from './MessageForm';
 import { Segment, Comment } from 'semantic-ui-react';
 
 class Messages extends React.Component {
