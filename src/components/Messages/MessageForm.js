30e79cd485d1d599b5595d4fabbb37ac8c38a781
3Mustard <m23.cullen@gmail.com>
2020-07-31 14:39:18 -0400

    added html and semantic components to messagesform

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,7 +4,30 @@ import { Segment, Button, Input } from 'semantic-ui-react';
 class MessageForm extends React.Component {
   render() {
     return(
-      <div></div>
+      <Segment>
+        <Input 
+          fluid
+          name="message"
+          style={{ marginBottom: '0.7em' }}
+          label={<Button icon={'add'} />}
+          labelPosition="left"
+          placeholder="Write your message"
+        />
+        <Button.Group icon widths="2">
+          <Button 
+            color="orange"
+            content="Add Reply"
+            labelPosition="left"
+            icon="edit"
+          />
+          <Button 
+            color="teal"
+            content="Upload Media"
+            labelPosition="right"
+            icon="cloud upload"
+          />
+        </Button.Group>
+      </Segment>
     )
   }
 }
