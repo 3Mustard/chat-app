b0ae9c5ef413c88c31683908b250ab1cc17faeec
3Mustard <m23.cullen@gmail.com>
2020-08-03 18:39:33 -0400

    set up open/close functions for a file modal component

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -8,8 +8,12 @@ class MessageForm extends React.Component {
     channel: this.props.currentChannel,
     user: this.props.currentUser,
     loading: false,
-    errors: []
+    errors: [],
+    modal: false
   }
+
+  openModal = () => this.setState({ modal: true });
+  closeModal = () => this.setState({ modal: false });
 
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value })
@@ -60,7 +64,7 @@ class MessageForm extends React.Component {
   }
 
   render() {
-    const { errors, message, loading } = this.state;
+    const { errors, message, loading, modal } = this.state;
   
     return (
       <Segment className="message__form">
@@ -90,9 +94,14 @@ class MessageForm extends React.Component {
           />
           <Button
             color="teal"
+            onClick={this.openModal}
             content="Upload Media"
             labelPosition="right"
             icon="cloud upload"
+          />
+          <FileModal 
+            modal={modal}
+            closeModal={this.closeModal}
           />
         </Button.Group>
       </Segment>
