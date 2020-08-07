8ff642deb43d449980e9431924ef8c34dbd724c6
3Mustard <m23.cullen@gmail.com>
2020-08-06 21:19:52 -0400

    fixed an error and added comments

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import firebase from '../../firebase';
-import { Menu, Icon } from 'semantic-ui-react';
+import React from "react";
+import firebase from "../../firebase";
+import { Menu, Icon } from "semantic-ui-react";
 
 class DirectMessages extends React.Component {
   state = {
     user: this.props.currentUser,
     users: [],
-    usersRef: firebase.database().ref('users'), // my users collection 
-    connectedRef: firebase.database().ref('info/connected'), //returns info on connected users
-    presenceRef: firebase.database().ref('presence') // my presence collection
-  }
+    usersRef: firebase.database().ref('users'), // Users collection
+    connectedRef: firebase.database().ref('.info/connected'), // updated every time the Firebase Realtime Database client's connection state changes
+    presenceRef: firebase.database().ref('presence') // Presence collection
+  };
 
   componentDidMount() {
     if (this.state.user) {
@@ -17,21 +17,21 @@ class DirectMessages extends React.Component {
     }
   }
 
-  // LISTENERS
+  // LISTENERS FOR DIRECT MESSAGES
   addListeners = currentUserUid => {
-    // UsersRef children ADDED
+    // USERS COLLECTION 
     let loadedUsers = [];
     this.state.usersRef.on('child_added', snap => {
       if (currentUserUid !== snap.key) {
         let user = snap.val();
-        user['uid'] = snap.key;
-        user['status'] = 'offline';
+        user["uid"] = snap.key;
+        user["status"] = "offline";
         loadedUsers.push(user);
         this.setState({ users: loadedUsers });
       }
     });
 
-    // ConnectedRef VALUE 
+    // CONNECTED REF
     this.state.connectedRef.on('value', snap => {
       if (snap.val() === true) {
         const ref = this.state.presenceRef.child(currentUserUid);
@@ -40,33 +40,33 @@ class DirectMessages extends React.Component {
           if (err !== null) {
             console.error(err);
           }
-        })
+        });
       }
     });
 
-    // PresenceRef children ADDED
+    // PRESENCE COLLECTION 
     this.state.presenceRef.on('child_added', snap => {
       if (currentUserUid !== snap.key) {
         this.addStatusToUser(snap.key);
       }
-    })
-    // PresenceRef children REMOVE 
+    });
+
     this.state.presenceRef.on('child_removed', snap => {
       if (currentUserUid !== snap.key) {
         this.addStatusToUser(snap.key, false);
       }
-    })
-  }
+    });
+  };
 
-  addStatusToUser = (userId, connected=true) => {
-    const updatedUsers = this.state.users.reduce((acc, user) =>{
+  addStatusToUser = (userId, connected = true) => {
+    const updatedUsers = this.state.users.reduce((acc, user) => {
       if (user.uid === userId) {
-        user['status'] = `${connected ? 'online' : 'offline'}`;
+        user["status"] = `${connected ? 'online' : 'offline'}`;
       }
       return acc.concat(user);
     }, []);
     this.setState({ users: updatedUsers });
-  }
+  };
 
   isUserOnline = user => user.status === 'online';
 
@@ -74,12 +74,12 @@ class DirectMessages extends React.Component {
     const { users } = this.state;
 
     return (
-      <Menu.Menu className="menu">
+      <Menu.Menu className='menu'>
         <Menu.Item>
           <span>
-            <Icon name="mail" /> DIRECT MESSAGES
-          </span>{' '}
-          ({ users.length })
+            <Icon name='mail' /> DIRECT MESSAGES
+          </span>{" "}
+          ({users.length})
         </Menu.Item>
         {users.map(user => (
           <Menu.Item
@@ -88,14 +88,14 @@ class DirectMessages extends React.Component {
             style={{ opacity: 0.7, fontStyle: 'italic' }}
           >
             <Icon
-              name="circle"
+              name='circle'
               color={this.isUserOnline(user) ? 'green' : 'red'}
             />
             @ {user.name}
           </Menu.Item>
         ))}
       </Menu.Menu>
-    )
+    );
   }
 }
 
