e7d9a3f50666320a29734d58f668c1ac4d0638cb
3Mustard <m23.cullen@gmail.com>
2020-08-07 23:49:19 -0400

    add function to highlight active direct message

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -6,6 +6,7 @@ import { Menu, Icon } from "semantic-ui-react";
 
 class DirectMessages extends React.Component {
   state = {
+    activeChannel: '',
     user: this.props.currentUser,
     users: [],
     usersRef: firebase.database().ref('users'), // Users collection
@@ -81,6 +82,7 @@ class DirectMessages extends React.Component {
     };
     this.props.setCurrentChannel(channelData);
     this.props.setPrivateChannel(true);
+    this.setActiveChannel(user.uid)
   }
 
   // Generates or gets a unique channel id
@@ -89,8 +91,12 @@ class DirectMessages extends React.Component {
     return userId < currentUserId ? `${userId}/${currentUserId}` : `${currentUserId}/${userId}`;
   }
 
+  setActiveChannel = userId => {
+    this.setState({ activeChannel: userId });
+  }
+
   render() {
-    const { users } = this.state;
+    const { users, activeChannel } = this.state;
 
     return (
       <Menu.Menu className='menu'>
@@ -103,6 +109,7 @@ class DirectMessages extends React.Component {
         {users.map(user => (
           <Menu.Item
             key={user.uid}
+            active={user.uid === activeChannel}
             onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: 'italic' }}
           >
