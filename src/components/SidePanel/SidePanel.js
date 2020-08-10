04009a1647c8bae9512daa0ac07d1e24fc4c8a28
3Mustard <m23.cullen@gmail.com>
2020-08-10 19:01:58 -0400

    copied displayChannels function to Starred.js

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Menu } from 'semantic-ui-react';
+
 import UserPanel from './UserPanel';
 import Starred from './Starred';
 import Channels from './Channels';
--- src/components/SidePanel/Starred.js
+++ src/components/SidePanel/Starred.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Menu, Icon } from 'semantic-ui-react';
 
 class Starred extends React.Component {
-  render() {
-    return (
+  state = {
+    activeChannel: '',
+    starredChannels: []
+  }
 
+  displayChannels = starredChannels => (
+    starredChannels.length > 0 && starredChannels.map(channel => (
+      <Menu.Item
+        key={channel.id}
+        onClick={() => this.changeChannel(channel)}
+        name={channel.name}
+        style={{ opacity: 0.7 }}
+        active={channel.id === this.state.activeChannel}
+      >
+        # {channel.name}
+      </Menu.Item>
+    ))
+  )
+
+  render() {
+    const { starredChannels } = this.state;
+
+    return (
+      <Menu.Menu className="menu">
+        <Menu.Item>
+          <span>
+            <Icon name="star" /> STARRED
+          </span>{" "}
+          ({ starredChannels.length })
+        </Menu.Item>
+        {/* Channels */}
+        {this.displayChannels(starredChannels)}
+      </Menu.Menu>
     )
   }
 }
