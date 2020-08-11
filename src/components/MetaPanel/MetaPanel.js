7990946ecec852f468e3b8f7be8f2f8fa53ca1ef
3Mustard <m23.cullen@gmail.com>
2020-08-11 15:01:43 -0400

    fixed whitespace/indentation

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -2,28 +2,28 @@ import React from 'react';
 import { Segment, Accordian, Header, Icon } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
-state = {
+  state = {
     activeIndex: 0
-}
-
-    render() {
-        const { activeIndex } = this.state;
-
-        return (
-            <Segment>
-                <Header as='h3' attached='top'>
-                    About # Channel
-                </Header>
-                <Accordian styled attached='true'>
-                    <Accordian.Title
-                        active={activeIndex === 0}
-                        index={0}
-                        onClick={this.setActiveIndex}
-                    ></Accordian.Title>
-                </Accordian>
-            </Segment>
-        )
-    }
+  }
+  
+  render() {
+    const { activeIndex } = this.state;
+  
+    return (
+      <Segment>
+        <Header as='h3' attached='top'>
+          About # Channel
+        </Header>
+        <Accordian styled attached='true'>
+          <Accordian.Title
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
+          ></Accordian.Title>
+        </Accordian>
+      </Segment>
+    )
+  }
 }
 
 export default MetaPanel;
