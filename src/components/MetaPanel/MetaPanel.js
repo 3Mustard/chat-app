300061dc419cfe7362f205810757ead60b59ee76
3Mustard <m23.cullen@gmail.com>
2020-08-11 15:49:14 -0400

    expanded on accordian content

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -24,11 +24,48 @@ class MetaPanel extends React.Component {
           About # Channel
         </Header>
         <Accordian styled attached='true'>
+
+          {/* INDEX 0 */}
           <Accordian.Title
             active={activeIndex === 0}
             index={0}
             onClick={this.setActiveIndex}
-          ></Accordian.Title>
+          > 
+            <Icon name='dropdown'/>
+            <Icon name='info'/>
+            Channel Details
+          </Accordian.Title>
+          <Accordian.Content active={activeIndex === 0}>
+              details
+          </Accordian.Content>
+
+          {/* INDEX 1 */}
+          <Accordian.Title
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown'/>
+            <Icon name='user circle'/>
+            Top Posters
+          </Accordian.Title>
+          <Accordian.Content active={activeIndex === 1}>
+            posters
+          </Accordian.Content>
+
+          {/* INDEX 2 */}
+          <Accordian.Title
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
+          >
+            <Icon name='dropdown'/>
+            <Icon name='pencil alternate'/>
+            Created By
+          </Accordian.Title>
+          <Accordian.Content active={activeIndex === 2}>
+            creator
+          </Accordian.Content>
         </Accordian>
       </Segment>
     )
