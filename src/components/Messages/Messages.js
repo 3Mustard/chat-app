db5a43e48539fe86d76d2652c68a94551c46f37a
3Mustard <m23.cullen@gmail.com>
2020-08-11 23:15:33 -0400

    connect Messages to store, add userposts to App props and pass them to metapanel

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { connect } from 'react-redux';
 import { Segment, Comment } from "semantic-ui-react";
 import firebase from '../../firebase';
+
+import { setUserPosts } from '../../actions';
 
 import MessagesHeader from "./MessagesHeader";
 import MessageForm from "./MessageForm";
@@ -199,4 +202,7 @@ class Messages extends React.Component {
   }
 }
 
-export default Messages;
+export default connect(
+  null,
+  { setUserPosts }
+)(Messages);
