85546e0aeceaa95ab5d3aac3e6ef8f55b16f8eef
3Mustard <m23.cullen@gmail.com>
2020-08-11 22:50:28 -0400

    added conditionals to render function in MetaPanel.js

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Segment, Accordion, Header, Icon } from 'semantic-ui-react';
+import { Segment, Accordion, Header, Icon, Image } from 'semantic-ui-react';
 
 class MetaPanel extends React.Component {
   state = {
-    privateChannel: this.props.isPrivateChannel,
-    activeIndex: 0
+    activeIndex: 0,
+    channel: this.props.currentChannel,
+    privateChannel: this.props.isPrivateChannel
   }
   
   // onClick(event: SyntheticEvent, data: object)
@@ -17,14 +18,14 @@ class MetaPanel extends React.Component {
   }
 
   render() {
-    const { activeIndex, isPrivateChannel } = this.state;
+    const { activeIndex, privateChannel, channel } = this.state;
 
-    if (privateChannel) return null;
+    if (privateChannel || !channel) return null;
   
     return (
-      <Segment>
+      <Segment loading={!channel}>
         <Header as='h3' attached='top'>
-          About # Channel
+          About # {channel && channel.name}
         </Header>
         <Accordion styled attached='true'>
 
@@ -39,7 +40,7 @@ class MetaPanel extends React.Component {
             Channel Details
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 0}>
-              details
+              {channel && channel.details}
           </Accordion.Content>
 
           {/* INDEX 1 */}
@@ -67,7 +68,10 @@ class MetaPanel extends React.Component {
             Created By
           </Accordion.Title>
           <Accordion.Content active={activeIndex === 2}>
-            creator
+            <Header as='h3'>
+              <Image circular src={channel && channel.createdBy.avatar}/>
+              {channel && channel.createdBy.name}
+            </Header>
           </Accordion.Content>
         </Accordion>
       </Segment>
