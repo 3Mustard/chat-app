eeca817575e7f878a49ab8a3f15c0869e2c0c015
3Mustard <m23.cullen@gmail.com>
2020-08-12 21:42:33 -0400

    added displayUserColors function and css rules for the html it returns

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -9,7 +9,24 @@ class ColorPanel extends React.Component {
     primary: '',
     secondary: '',
     user: this.props.currentUser,
+    userColors: [],
     usersRef: firebase.database().ref('users')
+  }
+
+  componentDidMount() {
+    if (this.state.user) {
+      this.addListener(this.state.user.uid);
+    }
+  }
+
+  addListener = userId => {
+    let userColors = [];
+    this.state.usersRef
+      .child(`${userId}/colors`)
+      .on('child_added', snap => {
+        userColors.unshift(snap.val());
+        this.setState({ userColors });
+      });
   }
 
   handleChangePrimary = color => this.setState({ primary: color.hex })
@@ -36,11 +53,24 @@ class ColorPanel extends React.Component {
       .catch(err => console.error(err));
   }
 
+  displayUserColors = colors => (
+    colors.length > 0 && colors.map((color, i) => (
+      <React.Fragment key={i}>
+        <Divider />
+        <div className="color__container">
+          <div className="color__square" style={{ background: color.primary }}>
+            <div className="color__overlay" style={{ background: color.secondary }}></div>
+          </div>
+        </div>
+      </React.Fragment>
+    ))
+  )
+
   openModal = () => this.setState({ modal: true })
   closeModal = () => this.setState({ modal: false })
 
   render() {
-    const { modal, primary, secondary } = this.state;
+    const { modal, primary, secondary, userColors } = this.state;
 
     return (
       <Sidebar
@@ -53,6 +83,7 @@ class ColorPanel extends React.Component {
       >
         <Divider />
         <Button icon="add" size="small" color="blue" onClick={this.openModal}/>
+        {this.displayUserColors(userColors)}
 
         {/* Color Picker Modal */}
         <Modal basic open={modal} onClose={this.closeModal}>
