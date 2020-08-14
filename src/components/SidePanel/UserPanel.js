0c67aa95bcfdc3d8c0f2d09278c1be16cc4e594d
3Mustard <m23.cullen@gmail.com>
2020-08-14 14:30:05 -0400

    added cropimage function

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -6,6 +6,8 @@ import { Button, Grid, Header, Icon, Dropdown, Image, Input, Modal } from 'seman
 class UserPanel extends React.Component {
 
     state = {
+        blob: '',
+        croppedImage: '',
         imagePreview: '',
         user: this.props.currentUser,
         modal: false
@@ -42,6 +44,18 @@ class UserPanel extends React.Component {
         }
     }
 
+    handleCropImage = () => {
+        if (this.avatarEditor) {
+            this.avatarEditor.getImageScaledToCanvas().toBlob(blob => {
+                let imageUrl = URL.createObjectURL(blob);
+                this.setState({
+                    croppedImage: imageUrl,
+                    blob
+                });
+            });
+        }
+    }
+
     handleSignout = () => {
         firebase    
             .auth()
@@ -50,7 +64,7 @@ class UserPanel extends React.Component {
     }
 
     render() {
-        const { user, modal, previewImage } = this.state;
+        const { user, modal, previewImage, croppedImage } = this.state;
         const { primaryColor } = this.props;
 
         return (
@@ -99,16 +113,23 @@ class UserPanel extends React.Component {
                                       )}
                                   </Grid.Column>
                                   <Grid.Column>
-                                      cropped Image Preview
+                                      {croppedImage && (
+                                          <Image 
+                                            style={{ margin: '3.5em auto' }}
+                                            width={100}
+                                            height={100}
+                                            src={croppedImage}
+                                          />
+                                      )}
                                   </Grid.Column>
                               </Grid.Row>
                           </Grid>
                       </Modal.Content>
                       <Modal.Actions>
-                          <Button color='green' inverted>
+                          {croppedImage && (<Button color='green' inverted>
                               <Icon name='save' /> Change Avatar 
-                          </Button>
-                          <Button color='green' inverted>
+                          </Button>)}
+                          <Button color='green' inverted onClick={this.handleCropImage}>
                               <Icon name='image' /> Preview
                           </Button>
                           <Button color='red' inverted onClick={this.closeModal}>
